8e0e8ee7f76300f0179379e0923727e9f4fd5bcc
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-06-07 16:05:30 -0700

    Place blocks where spirits are

--- src/main/java/com/skycatdev/spectralspirits/entity/SpectralSpiritEntity.java
+++ src/main/java/com/skycatdev/spectralspirits/entity/SpectralSpiritEntity.java
@@ -26,6 +26,7 @@ public class SpectralSpiritEntity extends MobEntity implements Ownable {
         noClip = true;
         owner = null;
         moveControl = new FlightMoveControl(this, 1, true);
+        intersectionChecked = false;
     }
 
     @Override
