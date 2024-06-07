4907ec8329d6bbb526f552032d07a62b155d72fa
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-06-06 18:58:07 -0700

    Project generation

---
+++ src/client/java/com/skycatdev/spectralspirits/SpectralSpiritsClient.java
@@ -0,0 +1,10 @@
+package com.skycatdev.spectralspirits;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SpectralSpiritsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/main/java/com/skycatdev/spectralspirits/SpectralSpiritsDataGenerator.java
@@ -0,0 +1,11 @@
+package com.skycatdev.spectralspirits;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SpectralSpiritsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
