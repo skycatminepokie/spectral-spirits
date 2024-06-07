d096affb2a900547e3885b27255bda9f45f11d3c
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-06-06 19:04:04 -0700

    Delete more example things

--- src/client/java/com/skycatdev/spectralspirits/SpectralSpiritsClient.java
+++ src/client/java/com/skycatdev/spectralspirits/SpectralSpiritsClient.java
@@ -5,6 +5,6 @@ import net.fabricmc.api.ClientModInitializer;
 public class SpectralSpiritsClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
-		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+
 	}
 }
