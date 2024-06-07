5fa6595cce9ff0274acad1fff2ab48358d28bb5d
skycatminepokie <52087591+skycatminepokie@users.noreply.github.com>
2024-06-06 19:06:17 -0700

    Make SpectralSpiritEntity

---
+++ src/main/java/com/skycatdev/spectralspirits/entity/SpectralSpiritEntity.java
@@ -0,0 +1,28 @@
+package com.skycatdev.spectralspirits.entity;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.data.DataTracker;
+import net.minecraft.nbt.NbtCompound;
+import net.minecraft.world.World;
+
+public class SpectralSpiritEntity extends Entity {
+    public SpectralSpiritEntity(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Override
+    protected void initDataTracker(DataTracker.Builder builder) {
+
+    }
+
+    @Override
+    protected void readCustomDataFromNbt(NbtCompound nbt) {
+
+    }
+
+    @Override
+    protected void writeCustomDataToNbt(NbtCompound nbt) {
+
+    }
+}
